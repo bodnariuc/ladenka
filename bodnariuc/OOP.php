5bf404d9dea0f4f25f53d5c29e0674ea26eb8340
bodnariuc <62306413+bodnariuc@users.noreply.github.com>
2020-09-23 11:47:26 +0200

    Update and rename oop.php to OOP.php

--- bodnariuc/OOP.php
+++ bodnariuc/OOP.php
@@ -1,7 +1,7 @@
-<?php
+<<?php
 
-//abstraktni trida ustredny a v ni protected trida $napetí
-abstract class  ustredny_Bodnariuc{
+//abstraktní trida ustredny obsahujici $napetí
+abstract class  ustredny_Bodnariuc {
 
     protected $napeti_Bodnariuc;
     //nastavi napeti
@@ -13,20 +13,19 @@ abstract class  ustredny_Bodnariuc{
         return $this-> napeti_Bodnariuc;
     }
 }
-// trida ustredna se dedi do abstraktni tridy ustredny
+// class ustredna se dedi do abstract class ustredny
 class  ustredna_Bodnariuc extends ustredny_Bodnariuc{
-   public $Napeti_Bodnariuc= 15;
-   //nastavi konstanty type
-   const TYPE ="12";
+   public $Napeti_Bodnariuc = 15;
+   //nastaveni konstanty type
+   const TYPE = "12";
 }
-//trida iustredna dedi interface od abstraktni tridy ustredny
+//class iustredna dedi interface od abstract class ustredny
 interface iustredna_Bodnariuc extends ustredny_Bodnariuc{
     //vypise nastavene hodnoty
     public function _contruct ($Napeti_Bodnariuc);
 }
 
-//pres vardummp vypise hodnotu v type a hodnotu ustrednoveho napeti
+//pres vardummp vypise hodnotu v type a hodnotu napeti ustredny
  var_dump (ustredna_Bodnariuc::TYPE);
  var_dump ($Napeti_Bodnariuc);
 ?>
-
